e0dd5becd7342b013b88e28848076a3e2a53074e
marc.pascual <marc.pascual@manomano.com>
2022-03-10 18:10:30 +0100

    Add index

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { magenta } from "chalk";
+import { findRandomGame } from "./finder/gameFinder"
+
+function start() {
+    const args = process.argv.slice(2);
+    const availableTime = args[0];
+    const game = findRandomGame(parseInt(availableTime))
+    console.log(`You can try ${magenta(game.name)}!`)
+}
+
+start()
