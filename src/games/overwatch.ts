c1d9e0fec97b4bc3d7c2797be0cbef3428bff2cd
marc.pascual <marc.pascual@manomano.com>
2022-03-11 11:39:16 +0100

    Format file

--- src/games/overwatch.ts
+++ src/games/overwatch.ts
@@ -1,9 +1,9 @@
-import { IGame } from "./IGame"
+import { IGame } from "./IGame";
 
-const overwatch: IGame ={
-    minDuration: 20,
-    maxDuration: 30,
-    name: "OverWatch"
-}
+const overwatch: IGame = {
+  minDuration: 20,
+  maxDuration: 30,
+  name: "OverWatch",
+};
 
 export default overwatch;
