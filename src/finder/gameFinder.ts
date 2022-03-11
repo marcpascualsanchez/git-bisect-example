74faafb79f27ba31f7e3be5071acea70b4dae74a
marc.pascual <marc.pascual@manomano.com>
2022-03-11 11:37:53 +0100

    Format file

--- src/finder/gameFinder.ts
+++ src/finder/gameFinder.ts
@@ -4,17 +4,19 @@ import { IGame } from "../games/IGame";
 import leagueOfLegends from "../games/leagueOfLegends";
 import lostArk from "../games/lostArk";
 
-const allGames = [leagueOfLegends, animalCrossing, battlefieldV, lostArk]
+const allGames = [leagueOfLegends, animalCrossing, battlefieldV, lostArk];
 
 function getRandom(list: any[]): any {
-    return list[Math.floor(Math.random() * list.length)]
+  return list[Math.floor(Math.random() * list.length)];
 }
 
 export function findRandomGame(availableTime: number): IGame {
-    let availableGames = allGames
-    if (availableTime) {
-        availableGames = allGames.filter(g => availableTime <= g.maxDuration && availableTime >= g.minDuration)
-    }
-    throw Error("I AM A NASTY BUG");
-    return getRandom(availableGames)
+  let availableGames = allGames;
+  if (availableTime) {
+    availableGames = allGames.filter(
+      (g) => availableTime <= g.maxDuration && availableTime >= g.minDuration
+    );
+  }
+  throw Error("I AM A NASTY BUG");
+  return getRandom(availableGames);
 }
