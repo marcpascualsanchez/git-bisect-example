eb96f60af6d70e167fc6c81afd7747d54d6ff850
marc.pascual <marc.pascual@manomano.com>
2022-03-11 11:38:43 +0100

    Format file

--- src/games/ageOfEmpires.ts
+++ src/games/ageOfEmpires.ts
@@ -1,9 +1,9 @@
-import { IGame } from "./IGame"
+import { IGame } from "./IGame";
 
-const ageOfEmpires: IGame ={
-    minDuration: 20,
-    maxDuration: 60,
-    name: "Age of Empires"
-}
+const ageOfEmpires: IGame = {
+  minDuration: 20,
+  maxDuration: 60,
+  name: "Age of Empires",
+};
 
 export default ageOfEmpires;
