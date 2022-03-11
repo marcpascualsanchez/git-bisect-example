2615b9a0020a7574e047c8e0f1b685349b9e834c
marc.pascual <marc.pascual@manomano.com>
2022-03-11 11:39:13 +0100

    Format file

--- src/games/marioKart.ts
+++ src/games/marioKart.ts
@@ -1,9 +1,9 @@
-import { IGame } from "./IGame"
+import { IGame } from "./IGame";
 
-const marioKart: IGame ={
-    minDuration: 5,
-    maxDuration: 20,
-    name: "Mario Kart 8"
-}
+const marioKart: IGame = {
+  minDuration: 5,
+  maxDuration: 20,
+  name: "Mario Kart 8",
+};
 
 export default marioKart;
