c20f47d3b9ece9a3dd21f77ba6f15c901b4f2199
marc.pascual <marc.pascual@manomano.com>
2022-03-11 11:40:21 +0100

    Add game to finder

--- src/finder/gameFinder.ts
+++ src/finder/gameFinder.ts
@@ -1,11 +1,12 @@
 import animalCrossing from "../games/animalCrossing";
 import battlefieldV from "../games/battlefieldV";
+import forza from "../games/forza";
 import { IGame } from "../games/IGame";
 import leagueOfLegends from "../games/leagueOfLegends";
 import lostArk from "../games/lostArk";
 import zelda from "../games/zelda";
 
-const allGames = [leagueOfLegends, animalCrossing, battlefieldV, lostArk, zelda];
+const allGames: IGame[] = [leagueOfLegends, animalCrossing, battlefieldV, lostArk, zelda, forza];
 
 function getRandom(list: any[]): any {
   return list[Math.floor(Math.random() * list.length)];
