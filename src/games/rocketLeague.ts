5a63551c1ae129bfd12543b9d53f7c9b12aa9a33
marc.pascual <marc.pascual@manomano.com>
2022-03-11 11:39:19 +0100

    Format file

--- src/games/rocketLeague.ts
+++ src/games/rocketLeague.ts
@@ -1,9 +1,9 @@
-import { IGame } from "./IGame"
+import { IGame } from "./IGame";
 
-const rocketLeague: IGame ={
-    minDuration: 20,
-    maxDuration: 40,
-    name: "Rocket League"
-}
+const rocketLeague: IGame = {
+  minDuration: 20,
+  maxDuration: 40,
+  name: "Rocket League",
+};
 
 export default rocketLeague;
